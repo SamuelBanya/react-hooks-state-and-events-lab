6b38a2cc0d76cec4900e689390807df1e51d10a4
Samuel Banya <sbanya@fastmail.com>
2022-06-29 21:14:35 -0400

    commit tree 907665c166a3f65e3b5df3cd1130ffc450f8fd45

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
+// Array Cheat Sheet:
+// Add: use the spread operator ([...])
+// Remove: use .filter
+// Update: use .map
+
 function ShoppingList({ items }) {
+  const [selectedCategory, changeCategory] = useState(false);
+
+  function selectEventHandler(event) {
+    console.log("event:");
+    console.log(event);
+
+    // const selectValue = event.target.value;
+
+    changeCategory(event.target.value);
+  }
+
+  const filteredList = items.filter(
+    (item) => item.category === selectedCategory
+  );
+
+  console.log("filteredList: ");
+  console.log(filteredList);
+
+  console.log("items:");
+  console.log(items);
+
+  // Use 'callback notation' to utilize 'setter' function to set the state accordingly:
+  // changeCategory(() => selectedCategory: filteredList);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={selectEventHandler} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +42,23 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {selectedCategory
+          ? filteredList.map((item) => (
+              <Item key={item.id} name={item.name} category={item.category} />
+            ))
+          : items.map((item) => (
+              <Item key={item.id} name={item.name} category={item.category} />
+            ))}
       </ul>
     </div>
   );
 }
+//         {cartStatus ? "Remove From Cart" : "Add to Cart"}
+
+// Previous 'items.map()' section inside JSX return statement:
+//         {items.map((item) => (
+//             <Item key={item.id} name={item.name} category={item.category} />
+//         ))
+// }
 
 export default ShoppingList;
