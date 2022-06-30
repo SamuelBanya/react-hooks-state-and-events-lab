d5856c0005035a870fd24bc5d774c0978621618a
Samuel Banya <sbanya@fastmail.com>
2022-06-29 21:14:40 -0400

    Adding latest changes to assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
-function App() {
+// Component Hierarchy:
+// └── src
+//     ├── __tests__
+//     │   ├── App.test.js
+//     │   ├── Item.test.js
+//     │   └── ShoppingList.test.js
+//     ├── components
+//     │   ├── App.js
+//     │   ├── Item.js
+//     │   └── ShoppingList.js
+//     ├── data
+//     │   └── items.js
+//     ├── index.css
+//     └── index.js
 
+function App() {
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+
+  const [isLight, setLightMode] = useState(false);
+
+  function changeLightMode() {
+    setLightMode((isLight) => !isLight);
+  }
+
+  // const appClass = false ? "App dark" : "App light";
+  const appClass = isLight ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={changeLightMode}>
+          {isLight ? "Dark Mode" : "Light Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cartStatus, setCartStatus] = useState(false);
+
+  function addedToCart() {
+    console.log("addedToCart() function called");
+
+    // setCartStatus((cartStatus) => !cartStatus);
+
+    // When the button is clicked, set the 'cartStatus' to 'in-cart':
+    console.log("setCartStatus() function called with callback syntax: ");
+    setCartStatus(() => (!cartStatus ? "in-cart" : ""));
+    // const temp = setCartStatus()
+    // change 'cartStatus' to 'in-cart':
+  }
+
   return (
-    <li className="">
+    <li className={cartStatus ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addedToCart} className="add">
+        {cartStatus ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
